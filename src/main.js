09e7ad73a10828dbf5877211528340b9cd0e37b7
vitipe <victorcasbas@gmail.com>
2021-03-23 10:01:23 -0300

    Reemplaza "Card titles" por "Cargando..." en el HTML

--- src/main.js
+++ src/main.js
@@ -4,16 +4,14 @@ function cargarPokemones() {
     .then(response => response.json())
     .then(dataAPI => {
 
-        document.querySelectorAll('.card-img-top').forEach((img, index) => {
-            img.src = `images/${index+1}.png`
-        })
-
         document.querySelectorAll('.card-title').forEach((title, index) => {
             title.textContent = dataAPI.results[index].name;
         })
+
+        document.querySelectorAll('.card-img-top').forEach((img, index) => {
+            img.src = `images/${index+1}.png`
+        })        
     })
-
-
 }
 
 cargarPokemones();
